4181f712bf54ea1da0928c9cda0d61bc578663d1
Yoav Klein <yoavklein25@gmail.com>
2020-10-20 22:54:50 +0300

    gitignore

--- functions.sh
+++ functions.sh
@@ -1,4 +1,12 @@
 
+function create_fs() {
+	sudo modprobe brd
+	sudo mkfs.ext2 /dev/ram0
+	sudo mount /dev/ram0 mnt
+	cd mnt
+	git clone https://github.com/yoav-klein/repo.git
+}
+
 function create_class() {
 	name=$1
 	hname=${name}.h
--- initdev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo modprobe brd
-sudo mkfs.ext2 /dev/ram0
-sudo mount /dev/ram0 mnt
